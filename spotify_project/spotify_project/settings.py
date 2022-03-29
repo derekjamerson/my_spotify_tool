e06a776013d91ae5bb4fb903c44d52412da8ce49
Derek Jamerson <derekjamerson@gmail.com>
2022-03-29 13:06:44 -0400

    artists added to db on pull

--- spotify_project/spotify_project/settings.py
+++ spotify_project/spotify_project/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'tracks',
     'django_extensions',
+    'tracks',
+    'artists',
 ]
 
 MIDDLEWARE = [
