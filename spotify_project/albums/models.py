01354c3c93b91e10a020cf1441533b21a2e764f6
Derek Jamerson <derekjamerson@gmail.com>
2022-03-29 20:35:57 -0400

    changed FK/M2M model reference to strings

--- spotify_project/albums/models.py
+++ spotify_project/albums/models.py
@@ -7,7 +7,7 @@ class Album(models.Model):
     objects = models.Manager()
     spotify_id = models.CharField(max_length=22, primary_key=True)
     name = models.CharField(max_length=200)
-    artists = models.ManyToManyField(Artist, related_name='albums')
+    artists = models.ManyToManyField('artists.Artist', related_name='albums')
 
     def __str__(self):
         return self.name
