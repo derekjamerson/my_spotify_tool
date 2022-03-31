8a83b5f91a28e01f7871cf844d41181bf02b5e49
Derek Jamerson <derekjamerson@gmail.com>
2022-03-31 11:54:16 -0400

    small change. reduce db hits

--- spotify_project/base/views.py
+++ spotify_project/base/views.py
@@ -44,5 +44,5 @@ def all_artists(request):
 
 def single_artist(request, artist_id):
     artist = get_object_or_404(Artist, spotify_id=artist_id)
-    tracks = Track.objects.filter(artists=artist_id).order_by(Lower('name'))
+    tracks = artist.tracks.order_by(Lower('name'))
     return render(request, 'single_artist.html', {'name': artist.name, 'tracks': tracks})
