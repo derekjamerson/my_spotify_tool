07c831b5ae59ec233bd3653804c7d6af72cfae29
Derek Jamerson <derekjamerson@gmail.com>
2022-04-04 16:43:35 -0400

    unfinished

---
+++ spotify_project/libraries/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models import CASCADE
+
+import users.models
+
+
+class Library(models.Model):
+    objects = models.Manager()
+    user = models.ForeignKey(users.models.CustomUser, on_delete=CASCADE, related_name='library')
