eb182ca063e9ebf3537d19586607b2d548ed0e6e
Derek Jamerson <derekjamerson@gmail.com>
2022-04-14 16:00:14 -0400

    in-process adding total_duration to library

--- spotify_project/libraries/urls.py
+++ spotify_project/libraries/urls.py
@@ -6,4 +6,10 @@ app_name = 'libraries'
 
 urlpatterns = [
     path(r'library_stats/<str:user_id>/', views.library_stats, name='library_stats'),
+    path(
+        r'library_stats/',
+        views.library_stats,
+        name='my_library_stats',
+        kwargs={'user_id': None},
+    ),
 ]
--- spotify_project/libraries/views.py
+++ spotify_project/libraries/views.py
@@ -3,5 +3,8 @@ from libraries.models import Library
 
 
 def library_stats(request, user_id):
-    library = get_object_or_404(Library, user=user_id)
+    if user_id is None:
+        library = request.user.library
+    else:
+        library = get_object_or_404(Library, user=user_id)
     return render(request, 'library_stats.html', {'library': library})
