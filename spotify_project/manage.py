d456b983483f938a2862c7404844fcc1f50192f6
Derek Jamerson <derekjamerson@gmail.com>
2022-04-17 21:45:33 -0400

    coveragerc added. manage.py modified to include coverage

--- spotify_project/manage.py
+++ spotify_project/manage.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotify_project.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spotify_project.settings")
+
+    # MyProject Customization: run coverage.py around tests automatically
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        command = "help"
+
+    running_tests = command == "test"
+    if running_tests:
+        from coverage import Coverage
+
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,6 +30,13 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+    if running_tests:
+        cov.stop()
+        cov.save()
+        covered = cov.report()
+        if covered < 100:
+            sys.exit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- spotify_project/runtests.sh
+++ spotify_project/runtests.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-./manage.py test $@ -v 2 --noinput
+./manage.py test "$@" -v 2 --noinput
