a4107a75f96836a1955bb277bbc0c38c52724bc9
Derek Jamerson <derekjamerson@gmail.com>
2022-04-20 13:32:36 -0400

    inprogress backends/base.py line 9-20

--- spotify_project/backends/tests/test_base.py
+++ spotify_project/backends/tests/test_base.py
@@ -1,8 +1,20 @@
+from unittest.mock import patch
+
 from testing import BaseTestCase
 from users.factories import CustomUserFactory
+from users.models import CustomUser
 
 
 class AuthBackendTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.user = CustomUserFactory()
+        self.user = CustomUserFactory.build()
+
+    def test_authenticate(self):
+        user_dict = {
+            'display_name': self.user.username,
+            'email': self.user.email,
+            'id': self.user.pk,
+        }
+        with patch('spotify.Spotify.fetch_current_user', return_value=user_dict):
+            self.assertTrue(self.user.is_authenticated)
