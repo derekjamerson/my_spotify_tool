2d6d9dfb684fd728abbbae8272bd550fbbab39ed
Derek Jamerson <derekjamerson@gmail.com>
2022-04-27 11:19:34 -0400

    undo changes

--- spotify_project/settings.py
+++ spotify_project/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -84,7 +83,12 @@ WSGI_APPLICATION = 'spotify_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
