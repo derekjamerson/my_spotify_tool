2803b50da06cc229875be1c34425f4aaea12c393
Derek Jamerson <derekjamerson@gmail.com>
2022-04-27 09:48:49 -0400

    using dj_database_url

--- spotify_project/settings.py
+++ spotify_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -83,12 +84,7 @@ WSGI_APPLICATION = 'spotify_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
