ac79d3f5fa7f681e2d2baf7eeba8ac948ee658f9
Derek Jamerson <derekjamerson@gmail.com>
2022-05-04 11:11:27 -0400

    updated tests

--- base/templatetags/diff_utils.py
+++ base/templatetags/diff_utils.py
@@ -7,7 +7,4 @@ register = template.Library()
 
 @register.simple_tag
 def calc_diff(thing_1, thing_2):
-    try:
-        return thing_1 - thing_2
-    except TypeError:
-        return timedelta()
+    return thing_1 - thing_2
