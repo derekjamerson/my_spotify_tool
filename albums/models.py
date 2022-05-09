41118bf6bf98076743d0ac393056881927861264
Derek Jamerson <derekjamerson@gmail.com>
2022-05-09 17:53:57 -0400

    move to postgres

--- albums/models.py
+++ albums/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Album(models.Model):
     objects = models.Manager()
-    spotify_id = models.CharField(max_length=22, primary_key=True)
+    spotify_id = models.CharField(max_length=100, primary_key=True)
     name = models.CharField(max_length=200)
     artists = models.ManyToManyField('artists.Artist', related_name='albums')
     release_date = models.CharField(max_length=10)
--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    spotify_id = models.CharField(max_length=22, primary_key=True)
+    spotify_id = models.CharField(max_length=100, primary_key=True)
